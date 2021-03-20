504b3c07657673c9a9c10f9360cc4c4534a07444
abe menashy <abestbet1@gmail.com>
2021-03-19 17:12:26 -0700

    comment quiz algorithm

--- components/Quiz.js
+++ components/Quiz.js
@@ -6,19 +6,23 @@ export default function Quiz({ dictionary }) {
   const [score, setScore] = useState(0);
   const [questionsList, setQuestionsList] = useState([]);
 
-  function randomInteger(min, max, i) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
   function randomIntegers(min, max, i) {
-    let rand = null; //an integer
+    let rand = null;
+    //will contain 3 index values
     let indexArray = [];
+    //run while loop
+    //1. rand === null , first iteration
+    //2. rand === i , if number equals i value
+    //3. if rand is already in the indexArray
+    //4. if the index length is less than 3
     while (
       (rand === null || rand === i || indexArray.includes(rand),
       indexArray.length < 3)
     ) {
+      //generate a random number
       rand = Math.floor(Math.random() * (max - min + 1)) + min;
 
+      //push into array
       if (rand !== i && !indexArray.includes(rand)) {
         indexArray.push(rand);
       }
@@ -40,14 +44,22 @@ export default function Quiz({ dictionary }) {
   };
 
   const startQuiz = () => {
+    //results in an array with format { english : string, french: string}
     let questionsArray = [];
     Object.values(dictionary).forEach((line) => questionsArray.push(...line));
+
     let questionAnswer = [];
 
     for (let i = 0; i < questionsArray.length; i++) {
+      //generate a random number from 0 to 3, to determine in which position
+      //the correct answer will be
       let randomInteger1 = Math.floor(Math.random() * 4);
+
+      //generate 3 random index numbers
       let [one, two, three] = randomIntegers(0, questionsArray.length - 1, i);
 
+      //create answer array with three false answers, use random index to
+      //determine answers
       let answerArray = [
         {
           answerText: questionsArray[one].english,
@@ -62,11 +74,13 @@ export default function Quiz({ dictionary }) {
           isCorrect: false,
         },
       ];
+      //based on random number chosen for the correct answer, splice the correct
+      //answer into the array
       answerArray.splice(randomInteger1, 0, {
         answerText: questionsArray[i].english,
         isCorrect: true,
       });
-
+      //push formatted object
       questionAnswer.push({
         questionText: questionsArray[i].french,
         answerOptions: answerArray,
