cf331f3c6eaa83b5c13977af43ee331820b8bc8f
abe menashy <abestbet1@gmail.com>
2021-05-10 22:26:05 -0700

    set up jest

---
+++ __tests__/index.spec.ts
@@ -0,0 +1,13 @@
+
+import * as React from 'react'
+
+import IndexPage from '../pages/index'
+
+describe('Pages', () => {
+  describe('Index', () => {
+    it('should render without throwing an error', function () {
+      // const wrap = mount(<IndexPage/>)
+      expect(2 + 2).toEqual(4)
+    })
+  })
+})
